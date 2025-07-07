69e1d122e6076fb9cd199eea7597dd96b829dc65
Karen-Palacios2007 <palaciosrojaskarendaniela@gmail.com>
2025-07-06 21:25:56 -0500

    Add files via upload
    Versión 1.1. ESTRUCTURA DE DATOS

--- formulario.py
+++ formulario.py
@@ -10,9 +10,13 @@ class Calculadora:
         return numero1,numero2
     
     def almacenarDatos(self,dato1,dato2):
-        dato_numero=[[dato1,dato2]]
+        dato_numero = [dato1, dato2]
+        objlista.guardar_dato(dato_numero)
         objlista.incluir_listas(dato_numero)
-        objlista.insertar_datos(dato_numero)
+        objlista.insertar_datos ()
+        objlista.eliminarPorIndice()
+        objlista.eliminarPorValor()
+        objlista.mostrarPosicion()
         
 calculadora=Calculadora()
 dato1,dato2=calculadora.pedir_numero()
@@ -21,11 +25,6 @@ calculadora.almacenarDatos(dato1,dato2)
 dato1,dato2=calculadora.pedir_numero()
 calculadora.almacenarDatos(dato1,dato2)
 
-dato1,dato2=calculadora.pedir_numero()
-calculadora.almacenarDatos(dato1,dato2)
-
-dato1,dato2=calculadora.pedir_numero()
-calculadora.almacenarDatos(dato1,dato2)
 
         
     
