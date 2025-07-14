bbab9eece896cba775fb156e173f3669ec9e1e92
Karen-Palacios2007 <palaciosrojaskarendaniela@gmail.com>
2025-07-14 15:56:55 -0500

    Add files via upload
    Estructura de Datos-Versión Actualizada

--- base_datos.py
+++ base_datos.py
@@ -8,26 +8,49 @@ class Base_Datos:
 # Se guardan los datos anteriormente digitados (.append())
     def guardarDatos(self,datoNumero):
         self.lista_numeros.append(datoNumero)
-        print(f"Lista con datos agregados: {self.lista_numeros}")
+        print(f"\nLista con datos agregados: {self.lista_numeros}")
         
 # Se inserta una nueva sublista para extenderla (.extend())
     def insertarLista(self,nuevaLista):
         self.lista_numeros.extend(nuevaLista)
-        print("La lista extendida es: ",self.lista_numeros)
+        print("\nLa lista extendida es: ",self.lista_numeros)
 
 # Se insertan datos a la lista, dependiendo de la posiciónque se indique (.insert())
     def almacenarDato(self,posicion,sublista):
         self.lista_numeros.insert(posicion,sublista)
-        print(self.lista_numeros)
+        print(f"\nLista con los nuevos datos insertados: {self.lista_numeros}")
 
 # Se elimina los datos de la sublista que se encuentre en la posición que se indique (.pop())    
     def eliminarDato(self,posicion,):
         numeroEliminado=self.lista_numeros.pop(posicion)
-        print(f"El dato {numeroEliminado} ha sido eliminado")
+        print(f"\nEl dato {numeroEliminado} ha sido eliminado")
         print(f"Lista actualizada: {self.lista_numeros}")
+           
+# Se elimina la sublista que contenga los números ingresados (.remove())
+    def remover(self,datos):
+        self.lista_numeros.remove(datos)
+        print("\nLista actualizada con datos removidos: ",self.lista_numeros)
+        
+# Muestra la posición de la sublista que contenga los números ingresados (.index())        
+    def mostrarPosicion(self,datos):
+        print(f"\nLos datos {datos} se encuentran en posición ", {self.lista_numeros.index(datos)})
+        
+# Muestra la posición de la sublista que contenga los números ingresados (.index())        
+    def mostrarContador(self,datos):
+        print(f"\nLos datos {datos} se repiten {self.lista_numeros.count(datos)} veces")
 
+# Ordena las sublistas ingresadas de forma ascendente (.sort())        
+    def ordenAscendente(self):
+        self.lista_numeros.sort()
+        print ("\nLa lista ordenada ascendentemente es: ", self.lista_numeros)
+
+# Ordena las sublistas ingresadas de forma descendente (.reverse())        
+    def ordenDescendente(self):
+        self.lista_numeros.reverse()
+        print ("\nLa lista ordenada descendentemente es: ", self.lista_numeros)
+        
 # Se muestra el elemento encontrado en el indice que se que se indique
     def verNumero(self,posicion):
         numero=self.lista_numeros.index(posicion)
-        return numero        
-    
+        return numero
+
--- formulario.py
+++ formulario.py
@@ -27,8 +27,8 @@ class Calculadora():
     
 # Se obtiene la posición en donde se desea insertar el dato ya determinado  
     def añadirDato(self):
-        Dato1=int(input("\nDato 1:"))
-        Dato2=int(input("Dato 2:"))
+        Dato1=input("\nDato 1:")
+        Dato2=input("Dato 2:")
         sublista=([Dato1,Dato2])
         posicion=int(input(f"\nDigite la posición en que desea insertar la sublista: ")) 
         lista.almacenarDato(posicion,sublista)          
@@ -46,7 +46,44 @@ class Calculadora():
                     print("\ningrese una valor dentro del rango e intente nuevamente...")
             except ValueError:
                 print("\ningrese una numero entero valido e intente nuevamente...")
-            
+                
+# Se remueve la sublista que contenga los datos digitados      
+    def removerDato(self):
+        while True:
+            try:
+                print("\nDigite los datos exactos de la sublista a eliminar ")
+                dato1=input("Dato 1:")
+                dato2=input("Dato 2:")
+                datos=([dato1,dato2])
+                lista.remover(datos)
+            except ValueError:
+                print("Los números no se han encontrado en una sublista, intente nuevamente:\n")
+
+# Se obtiene la posición de la sublista que contenga los datos digitados                 
+    def buscarIndice(self):
+        while True:
+            try:
+                print("\nDigite los datos exactos de la sublista que desea conocer la posición ")
+                dato1=input("Dato 1:")
+                dato2=input("Dato 2:")
+                datos=([dato1,dato2])
+                lista.mostrarPosicion(datos)
+            except ValueError:
+                print("Los números no se han encontrado en una sublista, intente nuevamente:\n")
+
+# Se obtiene la cantidad de veces que se repiten los datos de una sublista en otra              
+    def contador(self):
+        while True:
+            try:
+                print("\nDigite los datos exactos de la sublista que desea conocer la posición ")
+                dato1=input("Dato 1:")
+                dato2=input("Dato 2:")
+                datos=([dato1,dato2])
+                lista.mostrarContador(datos)
+            except ValueError:
+                print("Los números no se han encontrado en una sublista, intente nuevamente:\n")
+       
+    
 # Se obtiene la posición del dato que desea conocer
     def mostrarNumero(self):
         while True:
@@ -61,14 +98,22 @@ class Calculadora():
             except ValueError:
                 print("\ningrese una numero entero valido e intente nuevamente...")
 
-# Codigo principal
+#Se recibe la lista ordenada ascendentemente              
+    def mostrarAscendencia(self):
+        lista.ordenAscendente()
+        
+#Se recibe la lista ordenada descendentemente                      
+    def mostrarDescendencia(self):
+        lista.ordenDescendente()
+        
+        
+#***************************************CODIGO PRINCIPAL**************************************************
 
 # Se crea una instancia de la clase calculadora, que es quien contiene los metodos a ejecutar
 calculadora=Calculadora()
 
-aux=True
 # Se hace la creación del Menu
-while aux:    
+while True:    
     try:
         print("\nMENÚ")
         print("Digite el número de la acción que desea realizar:\n")
@@ -76,36 +121,38 @@ while aux:
         print("2. Eliminar un par de datos ya existente dependiendo de su posición")
         print("3. Insertar un nuevo par de datos")
         print("4. Remover un par de datos de acuerdo a su valores")
-        print("5. Salir del sistema ")
+        print("5. Conocer la posición de una sublista")
+        print("6. Contar y mostrar las veces que una sublista se repite")
+        print("7. Ordenar las sublistas ingresadas de forma ascendente")
+        print("8. Ordenar las sublistas ingresadas de forma descendente")
+        print("9. Salir del sistema ")
         print("Seleccione una opción: ")
         opc=input(" ")
+
+#Se hace uso de "match case" para verificar que la opción dígita sea valida para ejercutar una acción
         match opc:
             case "1":
-        #if opc=="1":
                 num1,num2=calculadora.pedirNumero()
-                calculadora.almacenarDatos(num1,num2)
-                
+                calculadora.almacenarDatos(num1,num2)                
             case "2":
-                numero=calculadora.eliminarNumero()
-                
+                numero=calculadora.eliminarNumero()                
             case "3":
-                dato=calculadora.añadirDato()
-                
+                dato=calculadora.añadirDato()                
+            case "4":
+                calculadora.removerDato()               
             case "5":
+                calculadora.buscarIndice()
+            case "6":
+                calculadora.contador()            
+            case "7":
+                calculadora.mostrarAscendencia()            
+            case "8":
+                calculadora.mostrarDescendencia()            
+            case "9":
                 print("Saliendo del sistema...")
-                aux = False
+                break
             case _:
-                print("Opción inválida, por favor ingrese un número válido del menú.")
-            
-
+                print("Opción inválida, por favor ingrese un número válido del menú.")    
     except ValueError:
             print("Opción invalida, ingrese un número de acuerdo al menu")
 
-    
-
-    
-
-# Se hace llamado de cada uno de los metodos para que las funciones se ejecuten
-
-#calculadora.agregarLista()
-#mostrar=calculadora.mostrarNumero()
